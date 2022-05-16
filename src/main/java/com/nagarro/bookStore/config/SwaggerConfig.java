f29364dca3253d2ff77feb71d7da356dc6d14152
jatin798 <jatin01398@gmail.com>
2022-05-16 17:18:09 +0530

    Correction in junit and swagger

--- src/main/java/com/nagarro/bookStore/config/SwaggerConfig.java
+++ src/main/java/com/nagarro/bookStore/config/SwaggerConfig.java
@@ -2,16 +2,21 @@ package com.nagarro.bookStore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@SuppressWarnings("deprecation")
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+@EnableSwagger2WebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public Docket postsApi() {
@@ -34,5 +39,12 @@ public class SwaggerConfig {
 
 				.licenseUrl("https://swagger.io/license/").version("1.0").build();
 
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
